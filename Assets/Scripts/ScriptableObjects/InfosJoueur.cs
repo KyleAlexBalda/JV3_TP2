08223eafc65327827ebfdd2d0b302f16e6efea0e
RenaudRoyBeaudoin <renaudroybeaudoin@gmail.com>
2023-11-24 20:18:41 -0500

    Mise en place du grab + script

---
+++ Assets/Scripts/ScriptableObjects/InfosJoueur.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName ="InfoJoueur", menuName = "SO/NouveauJoueur")]
+
+public class InfosJoueur : MonoBehaviour
+{   
+    //Contient le nombre de points actuels
+    public int _nbPoints;
+
+    //Contient le nombre de points totaux
+    public int _nbPointsTotaux;
+}
