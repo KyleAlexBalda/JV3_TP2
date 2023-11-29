1c673efbe8dc503899e85f39d701c02a88651384
RenaudRoyBeaudoin <renaudroybeaudoin@gmail.com>
2023-11-29 16:40:51 -0500

    Début LevelManager

---
+++ Assets/Scripts/Faits par Renaud/UIMainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField] Button _newGame;
+    [SerializeField] Button _quitGame;
+
+    private LevelManager _levelManager;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    _levelManager = LevelManager.Instance;
+
+    _newGame.onClick.AddListener(StarNewGame);   
+    _newGame.onClick.AddListener(QuitGame);   
+    }
+
+    private void StarNewGame(){
+       // _levelManager.LoadNewGame();
+       _levelManager.LoadAsyncScene("level01");
+    }
+    private void QuitGame(){
+        _levelManager.QuitGame();
+    }
+}
