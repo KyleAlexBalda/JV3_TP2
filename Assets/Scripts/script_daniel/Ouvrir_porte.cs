ac82f53f9aef7eefa2de63a7fecbac79f7841a90
daneli34 <daniel-rolon_3@hotmail.com>
2023-12-14 21:04:57 -0500

    fin scene Daniel

--- Assets/Scripts/script_daniel/Ouvrir_porte.cs
+++ Assets/Scripts/script_daniel/Ouvrir_porte.cs
@@ -10,12 +10,17 @@ public class Ouvrir_porte : MonoBehaviour
 
     private Animator _animator;
 
+    [SerializeField] private AudioSource _son;
+
+    private bool _pointsPris = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
         // Récupère le composant Animator
         _animator = GetComponent<Animator>();
+        _pointsPris = false;
     }
 
     private void Update()
@@ -23,12 +28,16 @@ public class Ouvrir_porte : MonoBehaviour
         ouvrePorte();
     }
     private void ouvrePorte(){
-        if(_infosJoueur._nbPointsTotaux == 8){
+        if(_infosJoueur._nbPointsTotaux == 8 && _pointsPris == false){
             Debug.Log("C'est ouvert");
             animPorte();
+            _son.Play();
+            _pointsPris = true;
         }
     }
     public void animPorte(){
         _animator.SetBool("porte_ouvre",true); 
     }
+
+
 }
